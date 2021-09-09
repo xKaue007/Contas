22c4f1a80914e31531d753242c0a0c4c9ead481f
xKaue007 <kaueoliveira92@gmail.com>
2021-09-09 19:08:49 +0000

    Saldo

---
+++ Conta.java
@@ -0,0 +1,9 @@
+public class Conta{
+
+    String titular;
+    int numero;
+    String dataValidade;
+    double saldo;
+
+
+}
---
+++ Principal.java
@@ -0,0 +1,12 @@
+public class Principal{
+    public static void main(String args[]){
+
+        Conta minhaConta= new Conta();
+
+         minhaConta.numero=12345;
+         minhaConta.saldo=20;
+
+    System.out.println(minhaConta.numero);
+    System.out.println(minhaConta.saldo);
+    }
+}
