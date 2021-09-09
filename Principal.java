456d8c4b0aba9780439129ad59108847d7bac1ba
xKaue007 <kaueoliveira92@gmail.com>
2021-09-09 19:44:25 +0000

    Conta

--- Principal.java
+++ Principal.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 public class Principal{
     public static void main(String args[]){
 
+        Scanner ler = new Scanner(System.in);
         Conta minhaConta= new Conta();
+        System.out.println("Digite seu nome: ");
+        minhaConta.titular = ler.nextLine();
+        
+        System.out.println("Digite o numero da conta: ");
+        minhaConta.numero = ler.nextInt();
 
-         minhaConta.numero=12345;
-         minhaConta.saldo=20;
+        System.out.println("Digite o seu saldo: ");
+        minhaConta.saldo = ler.nextDouble();
+        
 
+    System.out.println(minhaConta.titular);
     System.out.println(minhaConta.numero);
     System.out.println(minhaConta.saldo);
     }
